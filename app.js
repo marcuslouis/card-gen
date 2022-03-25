0b599dc49d54c7666e35e2c306868c48401dafcc
marcuslouis <Louis.Marcus@live.jobcorps.org>
2022-03-25 18:32:56 +0000

    card

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 let spade = [1,2,3,4,5,6,7,8,9,10,"K","Q","J","A"]
 let suit =["♦","♥","♠","♣"]
 let deck = suit[Math.floor(Math.random()*suit.length)]
- let arr = spade[Math.floor(Math.random()*spade.length)]
+let arr = spade[Math.floor(Math.random()*spade.length)]
 //  let result = Math.floor(Math.random() * 10)
 // document.getElementById("card").innerHTML = result
-document.querySelector("#card").innerHTML = arr
-document.querySelector("#cards").innerHTML = deck
+
+console.log(arr,deck)
+
+document.querySelector("#numbers").innerText = arr
+document.querySelector(".icon").innerText = deck
+document.querySelector(".icon2").innerText = deck
+// let div = document.getElementById("container");
+// div.style.color = "red";
+if(
+    deck === "♦"|| deck === "♥"
+)
+{
+    document.querySelector(".icon").style.color = "red";
+}else if(
+    deck === "♠"|| deck === "♣"
+)
+{
+    document.querySelector(".icon").style.color = "black";
+}
+if(
+    deck === "♦"|| deck === "♥"
+)
+{
+    document.querySelector(".icon2").style.color = "red";
+}else if(
+    deck === "♠"|| deck === "♣"
+)
+{
+    document.querySelector(".icon2").style.color = "black";
+}
